c80c6d2500568516be6b723f6e40e442c9475c5f
Haoyu Xu <kobale@HaoyudeMacBook-Pro.local>
2019-01-09 17:49:41 -0500

    2019.01.09

--- leetcode 146 LRU Cache.java
+++ leetcode 146 LRU Cache.java
@@ -2,85 +2,124 @@
 
 
 /*
-time: O()
-space: O()
+time: O(1)
+space: O(1)
 hashmap + double linkedlist
 */
 
 
 public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DLinkedNode post;
+    }
 
+    /**
+     * Always add the new node right after head;
+     */
+    private void addNode(DLinkedNode node) {
 
-	private class Node {
-		int key;
-		int value;
-		Node next;
-		Node pre;
-		Node(int k, int v) {
-			this.key = k;
-			this.value = v;
-		}
-		Node(){
-			this(0, 0);
-		}
-	}
-	private int capacity, count;
-	private Map<Integer, Node> map;
-	private Node head, tail;
-	
+        /*
+        Imagine we have a double linked list: " 1<-->3<-->4 ", and we need to 
+        insert a new node "2" in it so that it becomes "1<--> 2<-->3 <--> 4". 
+        After the first two line, we have "1 <-- 2 --> 3 <--> 4", 
+        where 1 is still pointing at 3 and 3 is still pointing at 1. 
+        Therefore, "head.post.pre = node" actually means "let 3 points to 2", 
+        and "head.post=node" means "let 1 points to 2".
+        */
+        node.pre = head;
+        node.post = head.post;
+
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    /**
+     * Remove an existing node from the linked list.
+     */
+    private void removeNode(DLinkedNode node) {
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    /**
+     * Move certain node in between to the head.
+     */
+    private void moveToHead(DLinkedNode node) {
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    // pop the current tail. 
+    private DLinkedNode popTail(){
+        DLinkedNode res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+
+    Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+    private int count;
+    private int capacity;
+    private DLinkedNode head, tail;
+
     public LRUCache(int capacity) {
-        this.capacity = capacity;
         this.count = 0;
-        map = new HashMap<>();
-        head = new Node();
-        tail = new Node();
-        head.next = tail;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        head.pre = null;
+
+        tail = new DLinkedNode();
+        tail.post = null;
+
+        head.post = tail;
         tail.pre = head;
     }
-    
+
     public int get(int key) {
-        Node n = map.get(key);
-        if(null == n) {
-        	return -1;
+
+        DLinkedNode node = cache.get(key);
+        if(node == null) {
+        return -1; // should raise exception here.
         }
-        update(n);
-        return n.value;
+
+        // move the accessed node to the head;
+        this.moveToHead(node);
+
+        return node.value;
     }
-    
-    public void set(int key, int value) {
-        Node n = map.get(key);
-        if(null == n) {
-        	n = new Node(key, value);
-        	map.put(key, n);
-        	add(n);
-        	++count;
-        } else {
-        	n.value = value;
-        	update(n);
-        }
+
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+ 
+        if(node == null) {
+
+        DLinkedNode newNode = new DLinkedNode();
+        newNode.key = key;
+        newNode.value = value;
+
+        this.cache.put(key, newNode);
+        this.addNode(newNode);
+
+        ++count;
+
         if(count > capacity) {
-        	Node toDel = tail.pre;
-        	remove(toDel);
-        	map.remove(toDel.key);
-        	--count;
+            // pop the tail
+            DLinkedNode tail = this.popTail();
+            this.cache.remove(tail.key);
+            --count;
+            }
+        } else {
+            // update the value.
+            node.value = value;
+            this.moveToHead(node);
         }
-    }
 
-    private void update(Node node) {
-    	remove(node);
-    	add(node);
-    }
-    private void add(Node node) {
-    	Node after = head.next;
-    	head.next = node;
-    	node.pre = head;
-    	node.next = after;
-    	after.pre = node;
-    }
-
-    private void remove(Node node) {
-    	Node before = node.pre, after = node.next;
-    	before.next = after;
-    	after.pre = before;
     }
 }
