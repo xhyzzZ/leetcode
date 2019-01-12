0c27370237ded2c6c0b205e1c28918559ce492f4
Haoyu Xu <kobale@HaoyudeMacBook-Pro.local>
2019-01-12 16:45:55 -0500

    2019.01.12

---
+++ leetcode 366 Find Leaves of Binary Tree.java
@@ -0,0 +1,34 @@
+//leetcode 366 Find Leaves of Binary Tree
+
+/*
+time: O(n ^ 2)
+space: O(h)
+*/
+
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> leavesList = new ArrayList< List<Integer>>();
+        List<Integer> leaves = new ArrayList<Integer>();
+        
+        while(root != null) {
+            if(isLeave(root, leaves)) root = null;
+            leavesList.add(leaves);
+            leaves = new ArrayList<Integer>();
+        }
+        return leavesList;
+    }
+    
+    public boolean isLeave(TreeNode node, List<Integer> leaves) {
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+            return true;
+        }
+        if (node.left != null) {
+             if(isLeave(node.left, leaves))  node.left = null;
+        }
+        if (node.right != null) {
+             if(isLeave(node.right, leaves)) node.right = null;
+        }
+        return false;
+    }
+}
---
+++ leetcode 813 Largest Sum of Averages.java
@@ -0,0 +1,27 @@
+//leetcode 813 Largest Sum of Averages
+
+/*
+time: O(N^3)
+space: O(N)
+*/
+
+class Solution {
+    public double largestSumOfAverages(int[] A, int K) {
+        int[] sum = new int[A.length];
+        for (int i = 0; i < A.length; i++) sum[i] = A[i] + (i > 0 ? sum[i - 1] : 0); 
+        double[] dp = new double[A.length];
+        
+        for (int groups = 1; groups <= K; groups++) {
+            for (int s = 0; s + groups <= A.length; s++) {
+                if (groups == 1) {
+                    dp[s] = ((double)(sum[A.length - 1] - sum[s] + A[s]) / (A.length - s));
+                    continue;
+                }
+                for (int e = s; e + groups <= A.length; e++) {
+                    dp[s] = Math.max(dp[s], (dp[e + 1] + (double) (sum[e] - sum[s] + A[s]) / (e - s + 1)));
+                }
+            }
+        }
+        return dp[0];
+    }
+}
