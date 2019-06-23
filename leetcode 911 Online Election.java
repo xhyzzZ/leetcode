fa1157c27b604827e7d752ce24d3444b1857b8d2
Haoyu Xu <xhy@bu.edu>
2019-06-23 19:42:00 +0800

    2019.06.24

---
+++ leetcode 911 Online Election.java
@@ -0,0 +1,17 @@
+//leetcode 911 Online Election
+
+/*
+time: O()
+space: O()
+*/
+
+class TopVotedCandidate {
+
+    public TopVotedCandidate(int[] persons, int[] times) {
+        
+    }
+    
+    public int q(int t) {
+        
+    }
+}
---
+++ leetcode 917 Reverse Only Letters.java
@@ -0,0 +1,19 @@
+//leetcode 917 Reverse Only Letters
+
+/*
+time: O(n)
+space: O(n)
+*/
+
+class Solution {
+    public String reverseOnlyLetters(String S) {
+        StringBuilder sb = new StringBuilder(S);
+        for (int i = 0, j = S.length() - 1; i < j; ++i, --j) {
+            while (i < j && !Character.isLetter(sb.charAt(i))) ++i;
+            while (i < j && !Character.isLetter(sb.charAt(j))) --j;
+            sb.setCharAt(i, S.charAt(j));
+            sb.setCharAt(j, S.charAt(i));
+        }
+        return sb.toString();
+    }
+}
