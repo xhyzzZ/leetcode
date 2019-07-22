db445c0bc49df2c0025de9f55a7e792f9a605f3f
Haoyu Xu <xhy@bu.edu>
2019-07-22 21:42:29 +0800

    2019.07.22

--- leetcode 271 Encode and Decode Strings.java
+++ leetcode 271 Encode and Decode Strings.java
@@ -1,19 +1,25 @@
 //leetcode 271 Encode and Decode Strings
 
 /*
-time: O()
-space: O()
+time: O(n)
+space: O(1)
 */
 
 public class Codec {
+  // Encodes a list of strings to a single string.
+  	public String encode(List<String> strs) {
+	    StringBuffer out = new StringBuffer();
+	    for (String s : strs)
+	        out.append(s.replace("#", "##")).append(" # ");
+	    return out.toString();
+	}
 
-    // Encodes a list of strings to a single string.
-    public String encode(List<String> strs) {
-        
-    }
-
-    // Decodes a single string to a list of strings.
-    public List<String> decode(String s) {
-        
-    }
+	public List<String> decode(String s) {
+	    List strs = new ArrayList();
+	    String[] array = s.split(" # ", -1);
+	    for (int i = 0; i < array.length - 1; ++i) {
+	        strs.add(array[i].replace("##", "#"));
+	    }
+	    return strs;
+	}
 }
---
+++ leetcode 709 To Lower Case.java
@@ -0,0 +1,18 @@
+//leetcode 709 To Lower Case
+
+/*
+time: O(n)
+space: O(n)
+*/
+
+class Solution {
+    public String toLowerCase(String str) {
+        char[] a = str.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            if ('A' <= a[i] && a[i] <= 'Z') {
+                a[i] = (char) (a[i] - 'A' + 'a');
+            }
+        }
+        return new String(a);
+    }
+}
