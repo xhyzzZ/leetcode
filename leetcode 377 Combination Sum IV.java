7bd4224b7c8deb51955da01da86ef2eae65fc76a
Haoyu Xu <xhy@bu.edu>
2019-07-23 20:35:22 +0800

    2019.07.23

---
+++ leetcode 377 Combination Sum IV.java
@@ -0,0 +1,21 @@
+//leetcode 377 Combination Sum IV
+
+/*
+time: O(n^2)
+space: O(n)
+*/
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] comb = new int[target + 1];
+	    comb[0] = 1;
+	    for (int i = 1; i < comb.length; i++) {
+	        for (int j = 0; j < nums.length; j++) {
+	            if (i - nums[j] >= 0) {
+	                comb[i] += comb[i - nums[j]];
+	            }
+	        }
+	    }
+	    return comb[target];
+    }
+}
