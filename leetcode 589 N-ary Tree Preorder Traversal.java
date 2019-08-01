2cc4aef718ef412c9ebf034f6bafcb5f4725db52
Haoyu Xu <xhy@bu.edu>
2019-08-01 20:58:45 +0800

    2019.08.01

---
+++ leetcode 589 N-ary Tree Preorder Traversal.java
@@ -0,0 +1,41 @@
+//leetcode 589 N-ary Tree Preorder Traversal
+
+/*
+time: O(n)
+space: O(h)
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+        
+        while (!stack.empty()) {
+            root = stack.pop();
+            list.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--) {
+                stack.add(root.children.get(i));
+            }
+        }
+        return list;
+    }
+}
+
+class Solution {
+    public List<Integer> list = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if (root == null) {
+            return list;
+        }
+        
+        list.add(root.val); 
+        for(Node node : root.children) {
+            preorder(node);
+        }
+                
+        return list;
+    }
+}
--- leetcode 95 Unique Binary Search Trees II.java
+++ leetcode 95 Unique Binary Search Trees II.java
@@ -3,19 +3,19 @@
 
 /*
 time: O(n)
-space: O()
+space: O(h)
 */
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
         List<TreeNode>[] res = new List[n + 1];
         res[0] = new ArrayList<>();
-        if(n == 0) return res[0];
+        if (n == 0) return res[0];
         res[0].add(null);
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
         	res[i] = new ArrayList<>();
-        	for(j = 0; j < i; j++) {
-        		for(TreeNode left : res[j]) {
-        			for(TreeNode root: res[i - j - 1]) {
+        	for (j = 0; j < i; j++) {
+        		for (TreeNode left : res[j]) {
+        			for (TreeNode root: res[i - j - 1]) {
         				TreeNode root = new TreeNode(j + 1);
         				root.left = left;
         				root.right = clone(right, i + 1);
@@ -28,40 +28,42 @@ public class Solution {
     }
 
     public TreeNode clone(TreeNode root, int k) {
-    	if(root == null) return root;
+    	if (root == null) return root;
     	TreeNode cur = new TreeNode(root.val + k);
     	cur.left = clone(root.left, k);
     	cur.right = clone(root.right, k);
     	return cur;
     }
+}
 
   
 /*
-time: O()
-space: O()
+time: O(n)
+space: O(h)
 */
+
+public class Solution {
     public List<TreeNode> generateTrees(int n) {
-    	if(n == 0) return new ArrayList<>();
-    	return genTreeList(1, n);
+        return genTreeList(1, n);
     }
 
-    public List<TreeNode> genTreeList(int start, int end) {
-    	List<TreeNode> list = new ArrayList<>();
-    	if(start > end) {
-    		list.add(null);
-    	}
-    	for(int idx = start; idx <= end; idx++) {
-    		List<TreeNode> leftlist = genTreeList(start, idx - 1);
-    		List<TreeNode> rightlist = genTreeList(idx + 1, end);
-    		for(TreeNode left : leftlist) {
-    			for(TreeNode right : rightlist) {
-    				TreeNode root = new TreeNode(idx);
-    				root.left = left;
-    				root.right = right;
-    				list.add(root);
-    			}
-    		}
-    	}
-    	return list;
+    private List<TreeNode> genTreeList (int start, int end) {
+        List<TreeNode> list = new ArrayList<TreeNode>(); 
+        if (start > end) {
+            list.add(null);
+        }
+        for (int idx = start; idx <= end; idx++) {
+            List<TreeNode> leftList = genTreeList(start, idx - 1);
+            List<TreeNode> rightList = genTreeList(idx + 1, end);
+            for (TreeNode left : leftList) {
+                for (TreeNode right: rightList) {
+                    TreeNode root = new TreeNode(idx);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
     }
 }
