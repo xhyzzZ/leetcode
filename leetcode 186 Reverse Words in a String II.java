e6fb31debf621c25444084a9f46ed10796846a6e
Haoyu Xu <xhy@bu.edu>
2019-08-03 22:57:18 +0800

    2019.08.03

--- leetcode 186 Reverse Words in a String II.java
+++ leetcode 186 Reverse Words in a String II.java
@@ -26,3 +26,28 @@ public class Solution {
 		}
 	}
 }
+
+
+public class Solution {
+	public void reverseWords(char[] s) {
+		int wordStart = 0;
+		int index = 0;
+		while (index < s.length) {
+			while (index < s.length && s[index] != ' ') index++;
+			reverse(s, wordStart, index - 1);
+			index ++;
+			wordStart = index;
+		}
+		reverse(s, 0, s.length - 1);
+	}
+	
+	private void reverse(char[] s, int start, int end) {
+		while (start < end) {
+			char temp = s[start];
+			s[start] = s[end];
+			s[end] = temp;
+			start ++;
+			end--;
+		}
+	}
+}
