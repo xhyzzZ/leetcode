881a73e77eae174166be0fcdf867b606f2f10915
xhyzzZ <xhy@bu.edu>
2019-08-05 20:44:40 +0800

    2019.08.04

--- leetcode 163 Missing Ranges.java
+++ leetcode 163 Missing Ranges.java
@@ -8,12 +8,12 @@ space: O(1)
 public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> res = new ArrayList<>();
-        for(int i : nums) {
-        	if(i > lower) res.add(lower + ((i - 1 > lower) ? "->" + (i - 1) : ""));
-        	if(i == upper) return res;
+        for (int i : nums) {
+        	if (i > lower) res.add(lower + ((i - 1 > lower) ? "->" + (i - 1) : ""));
+        	if (i == upper) return res;
         	lower = i + 1;
         }
-        if(lower <= upper) res.add(lower + ((upper > lower) ? "->" + (upper) : ""));
+        if (lower <= upper) res.add(lower + ((upper > lower) ? "->" + (upper) : ""));
         return res;
     }
 }
