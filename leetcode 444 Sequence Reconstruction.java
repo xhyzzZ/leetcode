a149824cc26f230b1ef57d31f5d264ff766c8a98
xhyzzZ <xhy@bu.edu>
2019-08-09 23:43:20 +0800

    2019.08.09

---
+++ leetcode 444 Sequence Reconstruction.java
@@ -0,0 +1,12 @@
+//leetcode 444 Sequence Reconstruction
+
+/*
+time: O()
+space: O()
+*/
+
+class Solution {
+    public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
+        
+    }
+}
