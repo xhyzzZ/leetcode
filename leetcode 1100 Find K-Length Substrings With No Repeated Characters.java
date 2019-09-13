0e982011091a2678d0134461419a41ce3c61f77a
xhyzzZ <xhy@bu.edu>
2019-09-13 15:25:05 -0400

    2019.09.13

---
+++ leetcode 1100 Find K-Length Substrings With No Repeated Characters.java
@@ -0,0 +1,27 @@
+//leetcode 1100 Find K-Length Substrings With No Repeated Characters
+
+/*
+time: O(n)
+space: O(1)
+*/
+
+class Solution {
+    public int numKLenSubstrNoRepeats(String S, int K) {
+        int ans = 0;
+        Set<Character> set = new HashSet<>();
+        int i = 0;
+        
+        for (int j = 0; j < S.length(); j++) {
+            while (set.contains(S.charAt(j))) {
+                set.remove(S.charAt(i++));
+            }
+            set.add(S.charAt(j));
+            
+            if (j - i + 1 == K) {
+                ans++;
+                set.remove(S.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
