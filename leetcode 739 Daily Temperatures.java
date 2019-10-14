77a54b357f9c3c422574ad3433122ec53b1f3848
xhyzzZ <xhy@bu.edu>
2019-10-13 23:34:54 -0400

    2019.10.13

--- leetcode 739 Daily Temperatures.java
+++ leetcode 739 Daily Temperatures.java
@@ -8,14 +8,14 @@ space: O(n)
 class Solution {
     public int[] dailyTemperatures(int[] T) {
         Stack<Integer> stack = new Stack<>();
-        int[] ret = new int[T.length];
-        for(int i = 0; i < T.length; i++) {
-        	while(!stack.isEmpty() && T[i] > T[stack.peek()]) {
+        int[] res = new int[T.length];
+        for (int i = 0; i < T.length; i++) {
+        	while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
         		int index = stack.pop();
-        		ret[index] = i - index;
+        		res[index] = i - index;
         	}
         	stack.push(i);
         }
-        return ret;
+        return res;
     }
 }
