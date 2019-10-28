8fbcea8b31654ed4e10b4e017c4cf034370f9f4d
xhyzzZ <xhy@bu.edu>
2019-10-27 23:22:54 -0400

    201910.27

--- algorithm/KMP Algroithm.java
+++ algorithm/KMP Algroithm.java
@@ -11,29 +11,29 @@ class KMP_String_Matching {
 		new KMP_String_Matching().KMPSearch(pat, txt); 
 	} 
 
-	public void KMPSearch(String pat, String txt) { 
-		int M = pat.length(); 
+	public void KMPSearch(String pattern, String txt) { 
+		int M = pattern.length(); 
 		int N = txt.length(); 
 
 		// create lps[] that will hold the longest 
 		// prefix suffix values for pattern 
-		int lps[] = new int[M]; 
+		int[] lps = new int[M]; 
 		int j = 0; // index for pat[] 
 
 		// Preprocess the pattern (calculate lps[] 
 		// array) 
-		computeLPSArray(pat, M, lps); 
+		lps = computeLPSArray(pattern); 
 
 		int i = 0; // index for txt[] 
 		while (i < N) { 
-			if (pat.charAt(j) == txt.charAt(i)) { 
+			if (pattern.charAt(j) == txt.charAt(i)) { 
 				j++; 
 				i++; 
 			} 
 			if (j == M) { 
 				System.out.println("Found pattern " + "at index " + (i - j)); 
 				j = lps[j - 1]; 
-			} else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+			} else if (i < N && pattern.charAt(j) != txt.charAt(i)) {
 				// mismatch after j matches 
 				// Do not match lps[0..lps[j-1]] characters, 
 				// they will match anyway 
@@ -41,37 +41,25 @@ class KMP_String_Matching {
 					j = lps[j - 1]; 
 				else
 					i = i + 1; 
-			} 
-		} 
+			}
+		}
 	} 
 
-	private void computeLPSArray(String pat, int M, int lps[]) { 
-		// length of the previous longest prefix suffix
-		int len = 0; 
-		int i = 1; 
-		lps[0] = 0; // lps[0] is always 0 
-
-		// the loop calculates lps[i] for i = 1 to M-1 
-		while (i < M) { 
-			if (pat.charAt(i) == pat.charAt(len)) { 
-				len++; 
-				lps[i] = len; 
-				i++; 
-			} else { 
-				// (pat[i] != pat[len]) 
-				// This is tricky. Consider the example. 
-				// AAACAAAA and i = 7. The idea is similar 
-				// to search step. 
-				if (len != 0) { 
-					len = lps[len - 1]; 
-					// Also, note that we do not increment 
-					// i here 
-				} else { 
-					// if (len == 0) 
-					lps[i] = len; 
-					i++; 
-				} 
-			} 
-		} 
+	private int[] computeLPSArray(String pattern) { 
+		int len = pattern.length();
+		int[] res = new int[len];
+		
+		int j = 0; 
+		for (int i = 1; i < pattern.length(); i++) {
+			while (j > 0 && pattern.charAt(j) != pattern.charAt(i)) {
+				j = res[j - 1];
+			}
+			
+			if (pattern.charAt(j) == pattern.charAt(i)) {
+				j++;
+			}
+			res[i] = j;
+		}
+		return res;
 	}
 } 
