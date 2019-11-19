62bcc1769fc298062c8496089ddd0248e1e22e30
xhyzzZ <xhy@bu.edu>
2019-11-18 21:19:50 -0800

    2019.11.18

--- leetcode 41 First Missing Positive.java
+++ leetcode 41 First Missing Positive.java
@@ -1,11 +1,29 @@
 //leetcode 41 First Missing Positive
 
-
-
 /*
 time: O(n)
 space: O(1)
 */
+public class Solution {
+	public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1)  return i + 1;
+        }
+        return nums.length + 1;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
 
 public class Solution {
     public int firstMissingPositive(int[] nums) {
