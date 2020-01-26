31bdd9c722aa2c754e015a4a7713b310f89322ce
xhyzzZ <xhy@bu.edu>
2020-01-25 19:59:51 -0500

    2020.01.25

--- leetcode 701 Insert into a Binary Search Tree.java
+++ leetcode 701 Insert into a Binary Search Tree.java
@@ -7,22 +7,41 @@ space; O(h)
 
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null) return new TreeNode(val);
-        TreeNode cur = root;
-        while(true) {
-            if(cur.val <= val) {
-                if(cur.right != null) cur = cur.right;
-                else {
-                    cur.right = new TreeNode(val);
-                    break;
+        TreeNode node = root;
+        while (node != null) {
+            // insert into the right subtree
+            if (val > node.val) {
+                // insert right now
+                if (node.right == null) {
+                    node.right = new TreeNode(val);
+                    return root;
+                } else {
+                    node = node.right;
                 }
             } else {
-                if(cur.left != null) cur = cur.left;
-                else {
-                    cur.left = new TreeNode(val);
-                    break;
+                // insert into the left subtree
+                // insert right now
+                if (node.left == null) {
+                    node.left = new TreeNode(val);
+                    return root;
+                } else {
+                    node = node.left;
                 }
             }
+        }
+        return new TreeNode(val);
+    }
+}
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
         }
         return root;
     }
