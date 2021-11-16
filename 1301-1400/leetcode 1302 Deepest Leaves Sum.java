b3a22d1f41535b13eba2b0bb3f0946ebd10bf879
Haoyu Xu <41977460+xhyzzZ@users.noreply.github.com>
2021-11-15 23:37:29 -0500

    2021.11.15

--- 1301-1400/leetcode 1302 Deepest Leaves Sum.java
+++ 1301-1400/leetcode 1302 Deepest Leaves Sum.java
@@ -2,9 +2,10 @@
 
 /*
 time: O(n)
-space: O(n)
+space: O(h)
 */
 
+bfs
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         if (root == null) return 0;
@@ -14,7 +15,7 @@ class Solution {
         while (!q.isEmpty()) {
             int size = q.size();
             sum = 0; // Reset for calculating the sum of elements of the next level
-            while (size-- > 0) {
+            for (int i = 0; i < size; i++) {
                 TreeNode top = q.poll();
                 sum += top.val;
                 if (top.left != null) q.offer(top.left);
@@ -22,5 +23,29 @@ class Solution {
             }
         }
         return sum;
+    }
+}
+
+dfs
+class Solution {
+     public int deepestLeavesSum(TreeNode root) {
+        int maxDepth = depth(root);
+        int[] res = {0};
+        findSum(root, maxDepth, 1, res);
+        return res[0];
+    }
+
+    private void findSum(TreeNode root, int maxDepth, int currentDepth, int[] res) {
+        if (root == null) return;
+        if (currentDepth == maxDepth) {
+            res[0] += root.val;
+        }
+        findSum(root.left, maxDepth, currentDepth + 1, res);
+        findSum(root.right, maxDepth, currentDepth + 1, res);
+    }
+    
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(depth(root.left), depth(root.right));
     }
 }
