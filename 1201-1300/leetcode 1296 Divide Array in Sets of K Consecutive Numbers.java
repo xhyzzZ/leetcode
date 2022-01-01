b1a08748f44a368541173ac77821ce5cab1c131a
Haoyu Xu <41977460+xhyzzZ@users.noreply.github.com>
2021-12-31 22:11:53 -0500

    2021.12.31

--- 1201-1300/leetcode 1296 Divide Array in Sets of K Consecutive Numbers.java
+++ 1201-1300/leetcode 1296 Divide Array in Sets of K Consecutive Numbers.java
@@ -1,37 +1,23 @@
 // leetcode 1296 Divide Array in Sets of K Consecutive Numbers
 
 /*
-time: O(nlogn)
+time: O(N+MlogM)
 space: O(n)
 */
 
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-        if (nums.length % k != 0) return false;
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        Queue<Integer> start = new LinkedList<>();
+        int last_checked = -1, opened = 0;
+        for (int i : map.keySet()) {
+            if (opened > 0 && i > last_checked + 1 || opened > map.get(i)) return false;
+            start.add(map.get(i) - opened);
+            last_checked = i; 
+            opened = map.get(i);
+            if (start.size() == k) opened -= start.remove();
         }
-        Collections.sort(list);
-        int time = 0;
-        
-        boolean flag = false;
-        while (list.size() != 0) {
-            int first = list.get(0);
-            list.remove(new Integer(first));
-            for (int i = 1; i < k; i++) {
-                if (list.contains(first + i)) {
-                    list.remove(new Integer(first + i));
-                } else {
-                    flag = true;
-                }
-            }
-            if (flag) {
-                return false;
-            }
-            time++;
-        }
-        if (time == nums.length / k) return true;
-        else return false;
+        return opened == 0;
     }
 }
